3bf57a7289e4d020e594c7a01b29911c35678877
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-08-17 13:22:27 -0700

    Print number update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,14 @@
  */
 void print_number(int n)
 {
+unsigned int num = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+num = -num;
 }
 
-if (n / 10 != 0)
+if ((n / 10) > 0)
 print_number(n / 10);
 
 _putchar(n % 10 + '0');
