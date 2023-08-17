f0e0f3109baaf19a45c22a06c9c47209f89beb60
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-08-17 12:45:46 -0700

    Square update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,19 +6,19 @@
  */
 void print_square(int size)
 {
-if (size <= 0)
-{
+int x, y;
+y = 0;
+if (size < 1)
 _putchar('\n');
-return;
-}
-
-int i, j;
-for (i = 0; i < size; i++)
+while (y < size)
 {
-for (j = 0; j < size; j++)
+x = 0;
+while (x < size)
 {
 _putchar('#');
+x++;
 }
 _putchar('\n');
+y++;
 }
 }
