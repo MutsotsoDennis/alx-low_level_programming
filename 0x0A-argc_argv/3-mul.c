77b034989c45a91e54c9021a915179e7a526dd9d
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-09-01 10:03:56 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,44 +8,31 @@
  * Return: the int converted from the string
  */
 int _atoi(char *s)
+
 {
-	int i, d, n, len, f, digit;
 
-	i = 0;
-
-	d = 0;
-
-	n = 0;
-
-	len = 0;
-
-	f = 0;
-
-	digit = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int len = 0;
 
 	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
+	len++;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+	while (i < len && (s[i] == ' ' || s[i] == '\t'))
+	i++;
+
+	if (s[i] == '-')
+	{
+	sign = -1;
+	i++;
 	}
 
-	if (f == 0)
-		return (0);
+	while (i < len && s[i] >= '0' && s[i] <= '9')
+	{
+	result = result * 10 + (s[i] - '0');
+	i++;
+	}
 
-	return (n);
+	return (result * sign);
 }
