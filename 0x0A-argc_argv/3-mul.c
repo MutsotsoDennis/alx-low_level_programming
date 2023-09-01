438f8106640aca866104c9cde6814f06625606a1
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-09-01 10:07:58 -0700

    Task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,31 +8,66 @@
  * Return: the int converted from the string
  */
 int _atoi(char *s)
-
 {
+	int i, d, n, len, f, digit;
 
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int len = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
 	while (s[len] != '\0')
-	len++;
+		len++;
 
-	while (i < len && (s[i] == ' ' || s[i] == '\t'))
-	i++;
-
-	if (s[i] == '-')
+	while (i < len && f == 0)
 	{
-	sign = -1;
-	i++;
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
 
-	while (i < len && s[i] >= '0' && s[i] <= '9')
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
-	result = result * 10 + (s[i] - '0');
-	i++;
+		printf("Error\n");
+		return (1);
 	}
 
-	return (result * sign);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
