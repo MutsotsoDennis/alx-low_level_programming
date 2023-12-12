dbfaec62ef13f99e6b604e3beed164684465543d
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-12-11 18:08:43 -0800

    File

---
+++ 0x18-dynamic_libraries/functions/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to the standard output
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+	putchar(*str);
+	str++;
+	}
+	putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
